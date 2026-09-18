2393a839e4039e0081c1069c7e9b45d8124e73bb
Nikkola <hi@nickchernobaev.com>
2026-09-18 20:09:45 +0300

    Кейсы: слайдер на owl carousel с перетаскиванием, как остальные

--- index.js
+++ index.js
@@ -245,6 +245,33 @@ try {
   console.log(e);
 }
 
+// Кейсы выпускников
+try {
+  $(document).ready(function () {
+    var owl = $(".cases__slider");
+    owl.owlCarousel({
+      loop: false,
+      margin: 12,
+      dots: false,
+      touchDrag: true,
+      mouseDrag: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 2,
+        },
+        1200: {
+          items: 3,
+        },
+      },
+    });
+  });
+} catch (e) {
+  console.log(e);
+}
+
 // VK player
 try {
   $(document).ready(function () {
